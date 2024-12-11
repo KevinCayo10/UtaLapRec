11be814dc77640163680494bdbcc0c0dd6280d46
Kevin CT <kcayo6155@uta.edu.ec>
2024-12-10 23:24:49 -0500

    refactor: Cambia el icono

--- src/modules/ui/Badge/Badge.jsx
+++ src/modules/ui/Badge/Badge.jsx
@@ -1,8 +1,8 @@
 import clsx from "clsx";
 
 function Badge({ children, ...props }) {
-  const baseStyles = `font-extrabold border-0  bg-red-500 badge badge-sm indicator-item
-                       sm:bg-red-500`;
+  const baseStyles = `font-extrabold border-0  bg-yellow-500 badge badge-sm indicator-item
+                       sm:bg-yellow-500`;
   return (
     <span className={clsx(baseStyles, props.className)} {...props}>
       {children}
