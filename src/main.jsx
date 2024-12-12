fd1a447da81c431daa5b2efd6358a71bccf24421
Kevin CT <kcayo6155@uta.edu.ec>
2024-12-12 10:08:38 -0500

    refactor: Ajusta para el deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Verificar si el entorno es producción para registrar el Service Worker
+if (process.env.NODE_ENV === "production") {
+  const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+  // Registrar el service worker
+  navigator.serviceWorker
+    .register(swUrl)
+    .then((registration) => {
+      console.log("Service Worker registrado con éxito:", registration);
+    })
+    .catch((error) => {
+      console.error("Error al registrar el Service Worker:", error);
+    });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
